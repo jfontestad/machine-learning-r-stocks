7046ce490026ac0fa3f6a155e9413ea2a53a2df0
damian44 <sergiovidela04@live.com>
2021-04-11 17:21:18 -0300

    added generators and data time bar functions

--- alpha_vantage.R
+++ alpha_vantage.R
@@ -44,7 +44,49 @@ getFullExtendedIntradaySeries <- function(symbol, interval, outputsize, datatype
 
 
 #2) darle pariodicidad que quiera, ticks, min, horas, dia, semanas, mes
+av_toperiod <- function(data, periodo, k = 1) {
+  #Espero data: "Time","Open","High","Low","Close","Volume"
+  #Periodos validos: "seconds", "minutes", "hours", "days", "weeks", "months", "quarters", and "years"
+  #k valido para intraday
+  data_xts <- xts(data[,2:ncol(data)], order.by = data[, 1])
+  data_xts <- to.period(data_xts, period = periodo, k = k)
+  colnames(data_xts) <- gsub("data_xts.", '', colnames(data_xts))
+  return(data_xts)
+}
 
 #3) preparar el genererator para el modelo de machine learning
+generator <- function(data, lookback, delay, min_index, max_index,
+                      shuffle = FALSE, batch_size = 128, step = 6) {
+  if (is.null(max_index)) {
+    max_index <- nrow(data) - delay - 1
+  }
+  i <- min_index + lookback
+  
+  function() {
+    if (shuffle) {
+      rows <- sample(c((min_index+lookback):max_index), size = batch_size)
+    } else {
+      if (i + batch_size >= max_index) {
+        i <<- min_index + lookback
+      }
+      rows <- c(i:min(i+batch_size, max_index))
+      i <<- i + length(rows)
+    }
+    
+    samples <- array(0, dim = c(length(rows),
+                                lookback/step,
+                                dim(data)[[-1]]))
+    targets <- array(0, dim = c(length(rows)))
+    
+    for (j in 1:length(rows)) {
+      indices <- seq(rows[[j]] - lookback, rows[[j]],
+                     length.out = dim(samples)[[2]])
+      samples[j,,] <- data[indices,]
+      targets[[j]] <- data[rows[[j]] + delay, 2]
+    }
+    
+    list(samples, targets)
+  }
+}
 
 #4) conectar el modelo probado en los otros scripts
--- model_01.R
+++ model_01.R
@@ -1,26 +1,25 @@
 #Alpha Vantage Dataset, LSTM model
+cat("\014")
+rm(list =ls())
+
 library(reticulate)
 library(keras)
 library(tensorflow)
 library(quantmod)
 library(reshape2)
+library(ggplot2)
 
-cat("\014")
-rm(list =ls())
 current_path <- rstudioapi::getActiveDocumentContext()$path
-setwd(dirname(dirname(current_path)))
+setwd(dirname(current_path))
 options(scipen = 999)
+print(getwd())
 
-source('MatbaRofexAPI/common/config.R', echo=TRUE)
-source('MatbaRofexAPI/common/db_read.R', echo=TRUE)
-source('MatbaRofexAPI/graficador/graphs.R', echo=TRUE)
-source('MatbaRofexAPI/rofex_api/rfx_rest.R', echo=TRUE)
-source('StockAnalisis/continuous_fut.R', echo=TRUE)
+source('alpha_vantage.R', echo=TRUE)
 
-Sys.setenv(RETICULATE_PYTHON = "C:/Users/sergio/Miniconda3/envs/py_36_64/python.exe")
-use_python("C:/Users/sergio/Miniconda3/envs/py_36_64/python.exe", required = TRUE)
-use_condaenv("C:/Users/sergio/Miniconda3/envs/py_36_64", required = TRUE)
-use_miniconda("py_36_64", required = TRUE)
+#Sys.setenv(RETICULATE_PYTHON = "C:/Users/sergio/Miniconda3/envs/py_36_64/python.exe")
+#use_python("C:/Users/sergio/Miniconda3/envs/py_36_64/python.exe", required = TRUE)
+#use_condaenv("C:/Users/sergio/Miniconda3/envs/py_36_64", required = TRUE)
+#use_miniconda("py_36_64", required = TRUE)
 reticulate::py_config()
 
 rangeEncoder <- function(timeseries, upper_limit, lower_limit, var_name) {
@@ -46,7 +45,7 @@ increaseDecreaseEncoder <- function(timeseries, var_name) {
 }
 
 aboveUnderEncoder <- function(timeseries1, timeseries2, var_name, var_name2) {
-  #Crea onde hot encoding para la comparacion entre dos series temporales: above, under
+  #Crea one hot encoding para la comparacion entre dos series temporales: above, under
   above <- ifelse(timeseries1[, 1] > timeseries2[, 1], 1, 0)
   under <- ifelse(timeseries1[, 1] <= timeseries2[, 1], 1, 0)
   encoder <-cbind.xts(above, under)
@@ -55,93 +54,108 @@ aboveUnderEncoder <- function(timeseries1, timeseries2, var_name, var_name2) {
   return(encoder)
 }
 
-
 #1) Data
-ggal <- getSymbols(Symbols = 'GGAL', auto.assign = FALSE)
-head(ggal)
-colnames(ggal) <- gsub("GGAL.", '', colnames(ggal))
+dataset <- read.csv("GGAL_intraday_series.csv")
+colnames(dataset) <- c("Time", "Open", "High", "Low", "Close", "Volume")
+dataset$Time <- as.POSIXct(as.character(dataset$Time))
+head(dataset)
+plot(dataset[1:2000, 'Close'])
+print(paste("dataset starting:", dataset$Time[nrow(dataset)], "and finishes:", dataset$Time[1]))
 
-fig <- plotly_chartSeries(ggal, 'GGAL')
-fig
-
+dataset_xts <- av_toperiod(dataset, periodo = "minutes", k = 5)
+dataset_xts$rtn <- diff(log(dataset_xts$Close))
+dataset_xts$rtn[is.na(dataset_xts$rtn)] <- 0.0
+dataset_xts$rtn <- dataset_xts$rtn * 100
+hist(dataset_xts$rtn, breaks = 500, probability = TRUE)
 
 #2) Indicadores
-dataset <- ggal
-dataset$sd <- TTR::volatility(dataset, calc = "yang.zhang", N = 20)
-
-plot.xts(dataset$sd)
-lines(xts(rep(0.3, length(index(dataset))), order.by = index(dataset)), col= "blue")
-lines(xts(rep(0.1, length(index(dataset))), order.by = index(dataset)), col= "red")
-
-dataset$ma_35 <- TTR::SMA(dataset$Close, n = 35)
-dataset$ma_5 <- TTR::SMA(dataset$Close, n = 5)
-dataset$ma_20 <- TTR::SMA(dataset$Close, n = 20)
-dataset$ewo <- dataset$ma_35 - dataset$ma_5
-
-hist(dataset$ewo, breaks = 200, probability = TRUE)
-
-dataset$rtn <- PerformanceAnalytics::Return.calculate(dataset$Close, method = 'log')
-hist(dataset$rtn, breaks = 500, probability = TRUE)
+# dataset <- ggal
+# dataset$sd <- TTR::volatility(dataset, calc = "yang.zhang", N = 20)
+# 
+# plot.xts(dataset$sd)
+# lines(xts(rep(0.3, length(index(dataset))), order.by = index(dataset)), col= "blue")
+# lines(xts(rep(0.1, length(index(dataset))), order.by = index(dataset)), col= "red")
+# 
+# dataset$ma_35 <- TTR::SMA(dataset$Close, n = 35)
+# dataset$ma_5 <- TTR::SMA(dataset$Close, n = 5)
+# dataset$ma_20 <- TTR::SMA(dataset$Close, n = 20)
+# dataset$ewo <- dataset$ma_35 - dataset$ma_5
+# 
+# hist(dataset$ewo, breaks = 200, probability = TRUE)
+# 
 
 #3)Encoders
-volEnc <- rangeEncoder(dataset$sd, 0.3, 0.1, 'vol')
-oscEnc <- increaseDecreaseEncoder(dataset$ewo, 'ewo')
-maEnc <- aboveUnderEncoder(dataset$Close, dataset$ma_20, 'close', 'ma_20')
-
-dataset <- cbind.xts(dataset, volEnc, oscEnc, maEnc)
+# volEnc <- rangeEncoder(dataset$sd, 0.3, 0.1, 'vol')
+# oscEnc <- increaseDecreaseEncoder(dataset$ewo, 'ewo')
+# maEnc <- aboveUnderEncoder(dataset$Close, dataset$ma_20, 'close', 'ma_20')
+# 
+# dataset <- cbind.xts(dataset, volEnc, oscEnc, maEnc)
 
 #4) target, train and test sets
-dataset$Adjusted <- NULL
-dataset$tomorrow_rtn <- lag(dataset$rtn, k= -1)
-d <- na.omit(dataset)
-features <- c('Open', 'High', 'Low', 'Close', 'Volume', 'rtn',
-              'low_vol', 'medium_vol', 'high_vol', 'increase_ewo', 
-              'decrease_ewo', 'close_above_ma_20', 'close_under_ma_20') #saco rtn para regression
-target <- 'tomorrow_rtn'
-x <- d[, features]
-y <- d[, target]
-d$y <- y
+data <- matrix(dataset_xts, 
+               nrow = nrow(dataset_xts), ncol = ncol(dataset_xts))
 
-#hyperparametars
-timesteps <- 1 #Unidades de 5 minutos que hacen a un dia operativo
-features_no <- dim(x)[2] #Atributos de data
-split <- round(nrow(d) * 0.7)
-batch_size <- 128
+colnames(data) <- colnames(dataset_xts)
+#scale sets
+train_index <- round(nrow(data) * 0.7)
+val_index <- round(nrow(data) * 0.85)
+train_data <- data[1:train_index, ]
+mean <- apply(train_data, 2, mean)
+std <- apply(train_data, 2, sd) 
+data <- scale(data, center= mean, scale = std)
 
-x_train <- x[1:split, ] 
-x_test  <- x[(split+1):nrow(d), ]
-#Target
-y_train <- y[1:split, ]
-y_test  <- y[(split+1):nrow(d), ]
-
+#hyperparameters
+lookback <- 35 #take data from 35 bars behind, 3 hours
+step <- 6 #sample every 30 mins (5*6)
+delay <- 15 #predict 15 bars ahead, 1.25 hours
+batch_size <- 128 
 # Large batches with few timesteps -> faster
 # Small batches with many timesteps -> better generalization/accuracy
 
-#scale sets
-x_train <- scale(x_train)
-col_means_train <- attr(x_train, "scaled:center") 
-cols_stddevs_train <- attr(x_train, "scaled:scale") 
-x_test <- scale(x_test, center= col_means_train,
-                scale = cols_stddevs_train)
-#Reshape [samples, timesteps. features]
-x_train <- array(x_train, c(dim(x_train)[1], timesteps, features_no))
-x_test <- array(x_test, c(dim(x_test)[1], timesteps, features_no))
+#generators
+train_gen <- generator(data,
+                       lookback = lookback,
+                       delay = delay,
+                       min_index = 1,
+                       max_index = train_index,
+                       shuffle = FALSE,
+                       step = step,
+                       batch_size = batch_size
+)
 
-# print(dim(x_train))
-# print(dim(y_train))
-# print(dim(x_test))
-# print(dim(y_test))
+val_gen <- generator(data,
+                       lookback = lookback,
+                       delay = delay,
+                       min_index = train_index + 1,
+                       max_index = val_index,
+                       shuffle = FALSE,
+                       step = step,
+                       batch_size = batch_size
+)
+
+test_gen <- generator(data,
+                       lookback = lookback,
+                       delay = delay,
+                       min_index = val_index + 1,
+                       max_index = NULL,
+                       shuffle = FALSE,
+                       step = step,
+                       batch_size = batch_size
+)
+
+val_steps <- (val_index - (train_index + 1) - lookback) / batch_size
+test_steps <- (nrow(data) - (val_index + 1) - lookback) / batch_size
 
 
 #5) Crear el modelo y entrenarlo
 
 model <- keras_model_sequential() %>%
   layer_lstm(units = 300,
-             input_shape = c(timesteps, features_no), return_sequences = TRUE) %>%
-  layer_dropout(0.5) %>%
-  layer_dense(units = 100, kernel_initializer= 'uniform') %>%
-  layer_dropout(0.5) %>%
-  layer_dense(units = 1, kernel_initializer= 'uniform') 
+             input_shape = list(NULL, dim(data)[[-1]])) %>%
+  #layer_dropout(0.5) %>%
+  layer_dense(units = 100) %>%
+  #layer_dropout(0.5) %>%
+  layer_dense(units = 1) 
 
 #predict_proba : sigmoid en output layer si quiero normalizar las predicciones como probabilidades y son dos clases 
 #para problema multi clase usar softmax,
@@ -150,51 +164,50 @@ model <- keras_model_sequential() %>%
 
 model %>%
   compile (
-    optimizer = 'adam',
+    optimizer = optimizer_rmsprop(),
     loss = 'mae', #mean squared error = mse mean absolute error =mae
     metrics = 'accuracy')
 
 summary(model)
 
-model %>%
-  fit(
-    x = x_train,
-    y = y_train,
-    epochs = 500,
-    validation_split = 0.2,
-    batch_size = batch_size
-  )
+history <- model %>% fit_generator(
+                      train_gen,
+                      steps_per_epoch= 500,
+                      epochs = 20,
+                      validation_data = val_gen,
+                      validation_steps = val_steps
+)
 
 #6) predecir y medir presicion
 #y_pred <- keras::predict_classes(model, x_test, batch_size = batch_size)
 #y_pred <- keras::predict_proba(model, x_test, batch_size = batch_size)
-y_pred <- model %>% predict(x_test)
-dim(y_pred)
-
-score <- model %>%
-  evaluate(
-    x_test,
-    y_test
-  )
-
-d$y_pred <- NA
-d$y_pred[(split+1):nrow(d), ] <- y_pred
-df <- data.frame(Date = index(d), Actual = d$y, Pred = d$y_pred)
-df <- na.omit(df)
-df$error <- df$y - df$y_pred
-print(mean(df$error))
-View(df)
-
-P <- tail(df, 10) %>% 
-  plot_ly(x = ~Date, y = ~y, name = 'actual', type = 'scatter', mode = 'markers') %>% 
-  add_trace(y = ~y_pred, name = 'prediction', mode = 'markers')
-P
-
-yesterday_d <- dataset[nrow(dataset), ]
-yesterday_d <- yesterday_d[, features]
-yesterday_d <- scale(yesterday_d, center= col_means_train,
-                     scale = cols_stddevs_train)
-yesterday_d <- array(yesterday_d, c(dim(yesterday_d)[1], timesteps, features_no))
-
-tommorrow_pred <- model %>% predict(yesterday_d)
-print(tommorrow_pred)
+# y_pred <- model %>% predict(x_test)
+# dim(y_pred)
+# 
+# score <- model %>%
+#   evaluate(
+#     x_test,
+#     y_test
+#   )
+# 
+# d$y_pred <- NA
+# d$y_pred[(split+1):nrow(d), ] <- y_pred
+# df <- data.frame(Date = index(d), Actual = d$y, Pred = d$y_pred)
+# df <- na.omit(df)
+# df$error <- df$y - df$y_pred
+# print(mean(df$error))
+# View(df)
+# 
+# P <- tail(df, 10) %>% 
+#   plot_ly(x = ~Date, y = ~y, name = 'actual', type = 'scatter', mode = 'markers') %>% 
+#   add_trace(y = ~y_pred, name = 'prediction', mode = 'markers')
+# P
+# 
+# yesterday_d <- dataset[nrow(dataset), ]
+# yesterday_d <- yesterday_d[, features]
+# yesterday_d <- scale(yesterday_d, center= col_means_train,
+#                      scale = cols_stddevs_train)
+# yesterday_d <- array(yesterday_d, c(dim(yesterday_d)[1], timesteps, features_no))
+# 
+# tommorrow_pred <- model %>% predict(yesterday_d)
+# print(tommorrow_pred)
