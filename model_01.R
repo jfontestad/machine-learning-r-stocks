d19f9c943835e4583faa6abc27b0d24edd1c2825
damian44 <sergiovidela04@live.com>
2021-05-20 00:44:52 -0300

    new input structure

--- model_01.R
+++ model_01.R
@@ -15,46 +15,15 @@ options(scipen = 999)
 print(getwd())
 
 source('alpha_vantage.R', echo=TRUE)
+source('utils.R', echo=TRUE)
 
 #Sys.setenv(RETICULATE_PYTHON = "C:/Users/sergio/Miniconda3/envs/py_36_64/python.exe")
 #use_python("C:/Users/sergio/Miniconda3/envs/py_36_64/python.exe", required = TRUE)
 #use_condaenv("C:/Users/sergio/Miniconda3/envs/py_36_64", required = TRUE)
 #use_miniconda("py_36_64", required = TRUE)
-reticulate::py_config()
+#reticulate::py_config()
 
-rangeEncoder <- function(timeseries, upper_limit, lower_limit, var_name) {
-  #Crea one hot encoding para tres regimenes de volatilidad: low, medium, high
-  low <- ifelse(timeseries[,1] <= lower_limit, 1, 0)
-  medium <- ifelse(timeseries[,1] > lower_limit & timeseries[, 1] < upper_limit, 1, 0)
-  high <- ifelse(timeseries[,1] >= upper_limit, 1, 0)
-  encoder <- cbind.xts(low, medium, high)
-  colnames(encoder) <- c(paste('low', var_name, sep = '_'),
-                         paste('medium', var_name, sep = '_'), 
-                         paste('high', var_name, sep = '_'))
-  return(encoder)
-}
-
-increaseDecreaseEncoder <- function(timeseries, var_name) {
-  #Crea one hot encoding para el estado de una serie temporal: increase, decrease
-  increase <- ifelse(timeseries[,1] - lag(timeseries[,1]) > 0, 1, 0) 
-  decrease <- ifelse(timeseries[,1] - lag(timeseries[,1]) <= 0, 1, 0)
-  encoder <- cbind.xts(increase, decrease)
-  colnames(encoder) <- c(paste('increase', var_name, sep = '_'),
-                         paste('decrease', var_name, sep = '_'))
-  return(encoder)
-}
-
-aboveUnderEncoder <- function(timeseries1, timeseries2, var_name, var_name2) {
-  #Crea one hot encoding para la comparacion entre dos series temporales: above, under
-  above <- ifelse(timeseries1[, 1] > timeseries2[, 1], 1, 0)
-  under <- ifelse(timeseries1[, 1] <= timeseries2[, 1], 1, 0)
-  encoder <-cbind.xts(above, under)
-  colnames(encoder) <- c(paste(var_name, 'above', var_name2, sep = '_'),
-                         paste(var_name, 'under', var_name2, sep = '_'))
-  return(encoder)
-}
-
-#1) Data
+#1) Data Preparation
 dataset <- read.csv("GGAL_intraday_series.csv")
 colnames(dataset) <- c("Time", "Open", "High", "Low", "Close", "Volume")
 dataset$Time <- as.POSIXct(as.character(dataset$Time))
@@ -68,94 +37,89 @@ dataset_xts$rtn[is.na(dataset_xts$rtn)] <- 0.0
 dataset_xts$rtn <- dataset_xts$rtn * 100
 hist(dataset_xts$rtn, breaks = 500, probability = TRUE)
 
-#2) Indicadores
-# dataset <- ggal
-# dataset$sd <- TTR::volatility(dataset, calc = "yang.zhang", N = 20)
-# 
-# plot.xts(dataset$sd)
-# lines(xts(rep(0.3, length(index(dataset))), order.by = index(dataset)), col= "blue")
-# lines(xts(rep(0.1, length(index(dataset))), order.by = index(dataset)), col= "red")
-# 
-# dataset$ma_35 <- TTR::SMA(dataset$Close, n = 35)
-# dataset$ma_5 <- TTR::SMA(dataset$Close, n = 5)
-# dataset$ma_20 <- TTR::SMA(dataset$Close, n = 20)
-# dataset$ewo <- dataset$ma_35 - dataset$ma_5
-# 
-# hist(dataset$ewo, breaks = 200, probability = TRUE)
-# 
-
-#3)Encoders
-# volEnc <- rangeEncoder(dataset$sd, 0.3, 0.1, 'vol')
-# oscEnc <- increaseDecreaseEncoder(dataset$ewo, 'ewo')
-# maEnc <- aboveUnderEncoder(dataset$Close, dataset$ma_20, 'close', 'ma_20')
-# 
-# dataset <- cbind.xts(dataset, volEnc, oscEnc, maEnc)
 
 #4) target, train and test sets
 data <- matrix(dataset_xts, 
-               nrow = nrow(dataset_xts), ncol = ncol(dataset_xts))
+               nrow = nrow(dataset_xts), 
+               ncol = ncol(dataset_xts))
 
 colnames(data) <- colnames(dataset_xts)
 #scale sets
-train_index <- 20000
-val_index <- 30000
-test_index <- 40000
-train_data <- data[1:train_index, ]
-mean <- apply(train_data, 2, mean)
-std <- apply(train_data, 2, sd) 
+train_index <- 30000
+#val_index <- 30000
+#test_index <- 40000
+train_mean_sd <- data[1:train_index, ]
+mean <- apply(train_mean_sd, 2, mean)
+std <- apply(train_mean_sd, 2, sd) 
 data <- scale(data, center= mean, scale = std)
 
+
 #hyperparameters
-lookback <- 78 #observaciones de un dia entero de trading en barras de 5 min 390 mins/5mins = 78 barras
-step <- 6 #muestras cada hora, 60 mins/ 5 = 12 barras
-delay <- 12 #objetivo es el retorno dos horas en el futuro
+lookback <- 12 #observaciones de un dia entero de trading en barras de 5 min 390 mins/5mins = 78 barras
+#step <- 6 #muestras cada hora, 60 mins/ 5 = 12 barras
+target <- 6 #objetivo es el retorno dos horas en el futuro
 batch_size <- 128 
 # Large batches with few timesteps -> faster
 # Small batches with many timesteps -> better generalization/accuracy
 # input size = (samples, timesteps, series) = (batch_size, lookback/step, features)
+train_data <- data[1:train_index,]
+test_data <- data[train_index:nrow(data),]
 
+train_set <- returnInputList(train_data, lookback, target)
+x_train <- train_set[[1]]
+y_train <- train_set[[2]]
+print("Train dims")
+print(dim(x_train)) 
+print(dim(y_train))
+
+test_set  <- returnInputList(test_data, lookback, target)
+x_test <- test_set[[1]]
+y_test <- test_set[[2]]
+print("Test dims")
+print(dim(x_train)) 
+print(dim(y_train))
 #generators
-train_gen <- generator(data,
-                       lookback = lookback,
-                       delay = delay,
-                       min_index = 1,
-                       max_index = train_index,
-                       shuffle = FALSE,
-                       step = step,
-                       batch_size = batch_size
-)
+# train_gen <- generator(data,
+#                        lookback = lookback,
+#                        delay = delay,
+#                        min_index = 1,
+#                        max_index = train_index,
+#                        shuffle = FALSE,
+#                        step = step,
+#                        batch_size = batch_size
+# )
 
 # test <- train_gen()
 # el1 <- test[[1]]
 # print(dim(el1))
 # el2 <- test[[2]]
 
-val_gen <- generator(data,
-                       lookback = lookback,
-                       delay = delay,
-                       min_index = train_index + 1,
-                       max_index = val_index,
-                       shuffle = FALSE,
-                       step = step,
-                       batch_size = batch_size
-)
-
-test_gen <- generator(data,
-                       lookback = lookback,
-                       delay = delay,
-                       min_index = val_index + 1,
-                       max_index = test_index,
-                       shuffle = FALSE,
-                       step = step,
-                       batch_size = batch_size
-)
+# val_gen <- generator(data,
+#                        lookback = lookback,
+#                        delay = delay,
+#                        min_index = train_index + 1,
+#                        max_index = val_index,
+#                        shuffle = FALSE,
+#                        step = step,
+#                        batch_size = batch_size
+# )
+# 
+# test_gen <- generator(data,
+#                        lookback = lookback,
+#                        delay = delay,
+#                        min_index = val_index + 1,
+#                        max_index = test_index,
+#                        shuffle = FALSE,
+#                        step = step,
+#                        batch_size = batch_size
+# )
 # test <- test_gen()
 # el1 <- test[[1]]
 # print(dim(el1))
 # el2 <- test[[2]]
 
-val_steps <- (val_index - (train_index + 1) - lookback) / batch_size
-test_steps <- (test_index - (val_index + 1) - lookback) / batch_size
+# val_steps <- (val_index - (train_index + 1) - lookback) / batch_size
+# test_steps <- (test_index - (val_index + 1) - lookback) / batch_size
 
 
 #5) Crear el modelo y entrenarlo
@@ -169,7 +133,7 @@ test_steps <- (test_index - (val_index + 1) - lookback) / batch_size
 #   layer_dense(units = 1) 
 
 model <- keras_model_sequential() %>%
-  layer_flatten(input_shape = c(lookback / step, dim(data)[-1])) %>%
+  layer_flatten(input_shape = c(dim(x_train)[2], dim(x_train)[3])) %>%
   layer_dense(units = 32, activation = "relu") %>%
   layer_dense(units = 1)
 
@@ -186,12 +150,11 @@ model %>%
 
 summary(model)
 
-history <- model %>% fit_generator(
-                      train_gen,
-                      steps_per_epoch= 500,
-                      epochs = 20,
-                      validation_data = val_gen(),
-                      validation_steps = 1
+history <- model %>% fit(x_train, y_train, 
+                         steps_per_epoch= 500,
+                         epochs = 20
+                         #validation_data = (x_test, y_test),
+                         #validation_steps = 1
 )
 
 predictions <- model %>% predict_generator(val_gen(), steps = 128)
---
+++ utils.R
@@ -0,0 +1,93 @@
+#Utils
+
+rangeEncoder <- function(timeseries, upper_limit, lower_limit, var_name) {
+  #Crea one hot encoding para tres regimenes de volatilidad: low, medium, high
+  low <- ifelse(timeseries[,1] <= lower_limit, 1, 0)
+  medium <- ifelse(timeseries[,1] > lower_limit & timeseries[, 1] < upper_limit, 1, 0)
+  high <- ifelse(timeseries[,1] >= upper_limit, 1, 0)
+  encoder <- cbind.xts(low, medium, high)
+  colnames(encoder) <- c(paste('low', var_name, sep = '_'),
+                         paste('medium', var_name, sep = '_'), 
+                         paste('high', var_name, sep = '_'))
+  return(encoder)
+}
+
+increaseDecreaseEncoder <- function(timeseries, var_name) {
+  #Crea one hot encoding para el estado de una serie temporal: increase, decrease
+  increase <- ifelse(timeseries[,1] - lag(timeseries[,1]) > 0, 1, 0) 
+  decrease <- ifelse(timeseries[,1] - lag(timeseries[,1]) <= 0, 1, 0)
+  encoder <- cbind.xts(increase, decrease)
+  colnames(encoder) <- c(paste('increase', var_name, sep = '_'),
+                         paste('decrease', var_name, sep = '_'))
+  return(encoder)
+}
+
+aboveUnderEncoder <- function(timeseries1, timeseries2, var_name, var_name2) {
+  #Crea one hot encoding para la comparacion entre dos series temporales: above, under
+  above <- ifelse(timeseries1[, 1] > timeseries2[, 1], 1, 0)
+  under <- ifelse(timeseries1[, 1] <= timeseries2[, 1], 1, 0)
+  encoder <-cbind.xts(above, under)
+  colnames(encoder) <- c(paste(var_name, 'above', var_name2, sep = '_'),
+                         paste(var_name, 'under', var_name2, sep = '_'))
+  return(encoder)
+}
+
+
+#Data generator
+generator <- function(data, lookback, delay, min_index, max_index,
+                      shuffle = FALSE, batch_size = 128, step = 6) {
+  if (is.null(max_index)) {
+    max_index <- nrow(data) - delay - 1
+  }
+  i <- min_index + lookback
+  
+  function() {
+    if (shuffle) {
+      rows <- sample(c((min_index+lookback):max_index), size = batch_size)
+    } else {
+      if (i + batch_size >= max_index) {
+        i <<- min_index + lookback
+      }
+      rows <- c(i:min(i+batch_size-1, max_index))
+      i <<- i + length(rows)
+      print(paste("start index", i, "end index", i + length(rows)))
+    }
+    
+    samples <- array(0, dim = c(length(rows),
+                                lookback/step,
+                                dim(data)[[-1]]))
+    targets <- array(0, dim = c(length(rows)))
+    
+    for (j in 1:length(rows)) {
+      indices <- seq(rows[[j]] - lookback, rows[[j]],
+                     length.out = dim(samples)[[2]])
+      samples[j,,] <- data[indices,]
+      targets[[j]] <- data[rows[[j]] + delay, ncol(data)] #asume que la columna target es la ultima columna (higher index)
+    }
+    
+    list(samples, targets)
+  }
+}
+
+returnInputList <- function(data, lookback, delay) {
+
+    samples <- array(0, dim = c(nrow(data) - lookback, #number of observations
+                                lookback,   #number of steps
+                                ncol(data))) #number of features
+    targets <- array(0, dim = c(nrow(data)) - lookback)
+    rows <- c(1 + lookback:nrow(data))
+    
+    for (i in 1:length(rows)) {
+      #browser()
+      #print(i)
+      if (rows[[i]] + delay - 1 > nrow(data)) {
+        break()
+      }
+      indices <- seq(rows[[i]] - lookback, rows[[i]] - 1,
+                     length.out = dim(samples)[[2]])
+      samples[i,,] <- data[indices,]
+      targets[[i]] <- data[rows[[i]] + delay - 1, ncol(data)] #asume que la columna target es la ultima columna (higher index)
+    }
+    
+    return(list(samples, targets))
+}
